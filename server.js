1f8a496474310ef64f6fdffcda8710f64260e2ca
User <imranbaganchara@gmail.com>
2019-11-04 03:43:49 +0600

    kdaj

--- server.js
+++ server.js
@@ -34,6 +34,4 @@ app.get('/profile/:id',(req,res)=>{profile.handleProfileGet(req,res,db)})
 app.put('/image',(req,res)=>{image.handleImage(req,res,db)})
 app.post('/imageurl',(req,res)=>{image.handleApiCall(req,res)})
 
-app.listen(3000,()=>{
-	console.log('app is working')
-})
+app.listen(3000)
